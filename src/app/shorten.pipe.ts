2661deed088e3982b699539c5a79b5ee1961700e
Chan, Aloysius <aloysius.chan@news.com.au>
2019-03-06 21:39:10 +1100

    - use custom pipe params

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -2,8 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'shorten'})
 export class ShortenPipe implements PipeTransform {
-    transform(value: any, ...args: any[]) {
-        const out = value.substr(0, 10);
+    transform(value: any, limit: number) {
+        const out = value.substr(0, limit);
         return out + (value.length > 10 ? ' ...' : '');
     }
 }
