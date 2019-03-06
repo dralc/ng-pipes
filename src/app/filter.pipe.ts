1a74f71dedaad097b5674fac219b6b4d8da47b43
Chan, Aloysius <aloysius.chan@news.com.au>
2019-03-06 22:21:27 +1100

    update type

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, filterString: string, prop: string): any {
+  transform(value: Array<Object>, filterString: string, prop: string): any {
     if (value.length === 0) {
       return;
     }
